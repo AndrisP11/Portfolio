09fcdc8b3ae701cab0b6f4fd7c2c82bea59b110e
AndrisP11 <110040171+AndrisP11@users.noreply.github.com>
2023-01-06 16:30:10 +0200

    took out apiKey

--- src/integration/github.ts
+++ src/integration/github.ts
@@ -2,36 +2,25 @@ import * as jsyaml from 'js-yaml';
 
 const API_NAME = 'https://api.github.com/users/{username}';
 
-export async function fetchNameData(username: string, apiKey: string | undefined) {
+export async function fetchNameData(username: string) {
     const endpoint = API_NAME.replace('{username}', username);
-    const res = await fetch(endpoint, {
-        headers: {
-            Authorization: `Bearer ${apiKey}`,
-        },
-    });
+    const res = await fetch(endpoint
+    );
     const dataName = await res.json();
     return dataName;
 }
 
-export async function fetchAllUserRepos(username: string, apiKey: string | undefined) {
-    const response = await fetch(`https://api.github.com/users/${username}/repos`, {
-        headers: {
-            Authorization: `Bearer ${apiKey}`,
-        },
-    });
+export async function fetchAllUserRepos(username: string) {
+    const response = await fetch(`https://api.github.com/users/${username}/repos`);
     const data = await response.json();
     return data;
 }
 
-export async function findReposWithYmlFile(username: string, ymlFilename: string, apiKey: string | undefined) {
-    const repos = await fetchAllUserRepos(username, apiKey);
+export async function findReposWithYmlFile(username: string, ymlFilename: string) {
+    const repos = await fetchAllUserRepos(username);
     const result = [];
     for (const repo of repos) {
-        const response = await fetch(`https://api.github.com/repos/${username}/${repo.name}/contents`, {
-            headers: {
-                Authorization: `Bearer ${apiKey}`,
-            },
-        });
+        const response = await fetch(`https://api.github.com/repos/${username}/${repo.name}/contents`);
         const data = await response.json();
         for (const file of data) {
             if (file.name === 'portfolio.yml') {
@@ -42,12 +31,8 @@ export async function findReposWithYmlFile(username: string, ymlFilename: string
     return result;
 }
 
-export async function fetchYmlFile(username: string, repoName: string, filePath: string, apiKey: string | undefined) {
-    const response = await fetch(`https://api.github.com/repos/${username}/${repoName}/contents/${filePath}`, {
-        headers: {
-            Authorization: `Bearer ${apiKey}`,
-        },
-    });
+export async function fetchYmlFile(username: string, repoName: string, filePath: string) {
+    const response = await fetch(`https://api.github.com/repos/${username}/${repoName}/contents/${filePath}`);
     const data = await response.json();
     return data;
 }
@@ -57,13 +42,13 @@ export async function parseYml(ymlString: string) {
 }
 
 
-export async function fetchYmlDataForRepos(username: string, repos: Array<{ name: string }>, ymlFilename: string, apiKey: string | undefined) {
+export async function fetchYmlDataForRepos(username: string, repos: Array<{ name: string }>, ymlFilename: string) {
     const result = [];
-    const ymlFiles = await findReposWithYmlFile(username, ymlFilename, apiKey);
+    const ymlFiles = await findReposWithYmlFile(username, ymlFilename);
 
     for (const repo of ymlFiles) {
         if (ymlFiles.length > 0) {
-            const ymlFile = await fetchYmlFile(username, repo.name, ymlFilename, apiKey);
+            const ymlFile = await fetchYmlFile(username, repo.name, ymlFilename);
             const ymlContent = atob(ymlFile.content);
             const ymlData = await parseYml(ymlContent) as { title: string, description: string, project_url: string, repo: string, owner: string, image_list: string[] };
             result.push(ymlData);
